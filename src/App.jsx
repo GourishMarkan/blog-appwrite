87af469d23b660f635ac12697fbf159d626a9c63
GourishMarkan <markangourish9@gmail.com>
2024-04-14 18:10:28 +0530

    Auth Service

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+
+import "./App.css";
+import conf from "./conf/conf";
+
+function App() {
+  // const [count, setCount] = useState(0);
+  console.log(conf.appwriteProjectId);
+
+  return (
+    <>
+      <h1>blog with appwrite</h1>
+    </>
+  );
+}
+
+export default App;
