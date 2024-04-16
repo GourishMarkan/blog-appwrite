45d402cf016c1b316ed6541a3889d2983d13b9c7
GourishMarkan <markangourish9@gmail.com>
2024-04-16 22:52:26 +0530

    state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-// import { useState } from "react";
-
+import { useState, UseEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
 import "./App.css";
-import conf from "./conf/conf";
+import { login, logout } from "./store/authSlice";
 
 function App() {
-  // const [count, setCount] = useState(0);
-  console.log(conf.appwriteProjectId);
-
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService.getCurrentUser().then((userData) => {
+      userData
+        ? dispatch(login())
+        : dispatch(logout)
+            .catch((e) => {
+              console.log("error is ::", e);
+            })
+            .finally(() => setLoading(false));
+    });
+  }, [loading]);
   return (
     <>
       <h1>blog with appwrite</h1>
